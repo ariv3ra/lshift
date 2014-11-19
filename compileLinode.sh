fadc3ef43ae28805aa9736696c7166814f1c2d1e
Angel Rivera <ariv3ra@gmail.com>
2014-11-19 00:09:47 -0500

    fixed the missing kernel in the final line

--- compileLinode.sh
+++ compileLinode.sh
@@ -12,7 +12,7 @@ touch /boot/grub/menu.lst
 
 echo "timeout 5">>/boot/grub/menu.lst
 echo "title Custom Compiled, kernel vmlinuz-3.17.3-custom">>/boot/grub/menu.lst
-echo "/boot/vmlinuz-3.17.3-custom root=/dev/xvda ro quiet security=selinux selinux=1">>/boot/grub/menu.lst
+echo "kernel /boot/vmlinuz-3.17.3-custom root=/dev/xvda ro quiet security=selinux selinux=1">>/boot/grub/menu.lst
 
 mv /boot/vmlinuz /boot/vmlinuz-3.17.3-custom
 
